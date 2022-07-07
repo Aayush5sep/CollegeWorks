ed37cba20e87163a17816dc9290b8766b04bdd83
unknown <goyalaayush93@gmail.com>
2022-07-07 11:48:43 +0530

    Profile Dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,10 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib import messages
+from user.models import UserDetails
 
 def profile(request):
-    return render(request,'dashboard/profile.html')
+    if request.user.is_authenticated:
+        userd=UserDetails.objects.get(user=request.user)
+        return render(request,'dashboard/profile.html',{'userd':userd})
+    else:
+        messages.error(request,"Kindly Log In")
+        return redirect("/user/loginpage")
 
 def dashboard(request):
-    return render(request,'dashboard/dashboard.html')
+    if request.user.is_authenticated:
+        return render(request,'dashboard/dashboard.html')
+    else:
+        messages.error(request,"Kindly Log In")
+        return redirect("/user/loginpage")
 
 def works(request):
-    return render(request,'dashboard/works.html')
+    if request.user.is_authenticated:
+        return render(request,'dashboard/works.html')
+    else:
+        messages.error(request,"Kindly Log In")
+        return redirect("/user/loginpage")
